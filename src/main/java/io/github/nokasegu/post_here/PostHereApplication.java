3a6834ef1a85e8832686ef7f56d35d405e1e3c0f
whffu762 <whffu762@naver.com>
2025-08-15 16:07:51 +0900

    start project

---
+++ src/main/java/io/github/nokasegu/post_here/PostHereApplication.java
@@ -0,0 +1,13 @@
+package io.github.nokasegu.post_here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nokasegu/post_here/PostHereApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nokasegu.post_here;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

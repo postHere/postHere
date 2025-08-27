bd653f2db51786681cac682619f26cd5971c9c1c
whffu762 <whffu762@naver.com>
2025-08-27 14:39:58 +0900

    [feat] : securityConfig 파일 작성 - 기본 세션 방식 사용 - 로그인 성공 처리 handler는 default로 사용 - 로그인 실패 처리 handler만 구현

---
+++ src/main/java/io/github/nokasegu/post_here/common/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package io.github.nokasegu.post_here.common.config;
+
+import io.github.nokasegu.post_here.common.security.CustomAuthenticationFailureHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String [] WHITELIST_URL = {
+      "/", "/signup/**", "/login", "/css/**", "/js/**"
+    };
+
+    private final CustomAuthenticationFailureHandler failureHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .usernameParameter("id")
+                        .passwordParameter("password")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureHandler(failureHandler)
+                        .permitAll()
+                );
+        http
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                );
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers(WHITELIST_URL).permitAll()
+                            .anyRequest().authenticated();
+                });
+
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

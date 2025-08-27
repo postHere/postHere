3aac5e8471a9cd49ffb821591804bd2ef29a3330
whffu762 <whffu762@naver.com>
2025-08-27 21:52:46 +0900

    [feat] : 응답 형식 변경 - JSON으로 오류 코드 및 메세지 보내는 방식으로 변경

--- src/main/java/io/github/nokasegu/post_here/common/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/io/github/nokasegu/post_here/common/security/CustomAuthenticationFailureHandler.java
@@ -1,27 +1,38 @@
 package io.github.nokasegu.post_here.common.security;
 
-import io.github.nokasegu.post_here.common.exception.ErrorMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.nokasegu.post_here.common.dto.WrapperDTO;
+import io.github.nokasegu.post_here.common.exception.Code;
+import io.github.nokasegu.post_here.userInfo.domain.UserInfoEntity;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
-        String errorCode = ErrorMessage.LOGIN_FAIL.getCode();
-        String errorMessage = ErrorMessage.LOGIN_FAIL.getValue();
-        String location = "/login?code=" + URLEncoder.encode(errorCode, StandardCharsets.UTF_8)
-                + "&message=" + URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
-        response.sendRedirect(location);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        UserInfoEntity userInfoEntity = new UserInfoEntity();
+        WrapperDTO<UserInfoEntity> error = new WrapperDTO<>();
+        error.setStatus(Code.LOGIN_FAIL.getCode());
+        error.setMessage(Code.LOGIN_FAIL.getValue());
+
+        error.setData(userInfoEntity);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(error));
     }
 }

ce2f487ea0203dae155e9132c684b05c2be7bc72
whffu762 <whffu762@naver.com>
2025-08-28 15:07:43 +0900

    [feat] : 입력 예외 처리 - ID, PW 입력 안하면 요청 보내지 않음

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,28 +1,40 @@
 $(document).ready(function () {
 
-    $('form').on('submit', function (event) {
-
-        event.preventDefault();
-        $('#login-error').text('');
-
-        const username = $('#id').val();
-        const password = $('#password').val();
-
-        const loginData = {
-            id: username,
-            password: password
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "/login",
-            data: loginData,
-        })
-            .done(function (response) {
-                window.location.href = "/";
-            })
-            .fail(function (xhr, status, error) {
-                $('#login-error').text(message);
-            });
-    });
+    $('form').on('submit', tryLogin)
 });
+
+function tryLogin(event) {
+
+    event.preventDefault();
+    $('#login-error').text('');
+
+    const username = $('#id').val();
+    const password = $('#password').val();
+
+    if (!username || !password) {
+
+        const message = '아이디와 비밀번호를 모두 입력해주세요';
+        $('#login-error').text(message);
+        return false;
+    }
+
+    const loginData = {
+        id: username,
+        password: password
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/login",
+        data: loginData,
+        datatype: "json"
+    })
+        .done(function (response) {
+            window.location.href = "/";
+        })
+        .fail(function (xhr, status, error) {
+
+            const message = xhr.responseJSON.message;
+            $('#login-error').text(message);
+        });
+}

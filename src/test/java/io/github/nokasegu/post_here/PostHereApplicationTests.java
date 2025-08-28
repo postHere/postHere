1f9c26021c400c62ffb54ab8716950aea8ec01c4
whffu762 <whffu762@naver.com>
2025-08-28 15:06:10 +0900

    [test] : 테스트용 비밀번호 생성기 - PasswordEncoder를 이용해서 비밀번호를 인코딩해주는 메소드

--- src/test/java/io/github/nokasegu/post_here/PostHereApplicationTests.java
+++ src/test/java/io/github/nokasegu/post_here/PostHereApplicationTests.java
@@ -1,15 +1,27 @@
 package io.github.nokasegu.post_here;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class PostHereApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void generatePassword() {
+
+        String raw = "1111";
+        String hash = passwordEncoder.encode(raw);
+        System.out.println("111" + hash);
+    }
 }

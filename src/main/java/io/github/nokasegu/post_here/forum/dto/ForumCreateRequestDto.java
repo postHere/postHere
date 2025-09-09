fd54ff2ab90a1537198b62eeb6d8a0603b1f781e
whffu762 <whffu762@naver.com>
2025-09-09 17:14:32 +0900

    [chore] : 오탈자 수정

--- src/main/java/io/github/nokasegu/post_here/forum/dto/ForumCreateRequestDto.java
+++ src/main/java/io/github/nokasegu/post_here/forum/dto/ForumCreateRequestDto.java
@@ -1,10 +1,7 @@
 package io.github.nokasegu.post_here.forum.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 

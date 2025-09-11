9a9e79b99fb30df39fa1d250e3f94dcc267cbf3a
kubell <whdtlr98@naver.com>
2025-09-11 14:15:45 +0900

    [feat] : park 레포지토리 수정
    park 정보조회용 메서드 추가
    
    Refs #48

--- src/main/java/io/github/nokasegu/post_here/park/repository/ParkRepository.java
+++ src/main/java/io/github/nokasegu/post_here/park/repository/ParkRepository.java
@@ -4,6 +4,15 @@ import io.github.nokasegu.post_here.park.domain.ParkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParkRepository extends JpaRepository<ParkEntity, Long> {
+    /**
+     * 소유자(owner)의 ID를 기반으로 Park 정보를 조회합니다.
+     *
+     * @param ownerId Park 소유자의 User ID
+     * @return Optional<ParkEntity>
+     */
+    public Optional<ParkEntity> findByOwnerId(Long ownerId);
 }

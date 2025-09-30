808593e2a93fa149798dba22ea074339ad099be1
KIMTISU <kjs478100@gmail.com>
2025-10-01 03:20:48 +0900

    [fix]: 프로필 fin'd css 수정
    - FindPostSummaryDto에 시간 설정을 위한 데이터 추가
    - FindService에 시간 값을 받아오기 위한 로직 추가
    - 작성시간과 유효기간 css 및 js 수정 및 작성
    
    Refs #184

--- src/main/java/io/github/nokasegu/post_here/find/dto/FindPostSummaryDto.java
+++ src/main/java/io/github/nokasegu/post_here/find/dto/FindPostSummaryDto.java
@@ -3,6 +3,8 @@ package io.github.nokasegu.post_here.find.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class FindPostSummaryDto {
@@ -10,4 +12,8 @@ public class FindPostSummaryDto {
     private String imageUrl;
     private String location; // TODO: 좌표를 주소로 변환하는 로직 필요
     private boolean isExpiring;
+
+    // 필요한 원본 시간 데이터 추가
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
 }
